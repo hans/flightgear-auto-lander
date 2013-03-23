e0d49f3ce6493140a3aa527b75c482e1d642f30b
Hans Engel <engel@engel.uk.to>
2013-03-22 22:55:14 -0700

    Depend on hans/flightgear instead.. I'm sure we'll end up modding this :)

--- project.clj
+++ project.clj
@@ -3,6 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :plugins [[lein-git-deps "0.0.1-SNAPSHOT"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.novemberain/monger "1.5.0-rc1"]
-                 [org.clojars.dalethatcher/flightgear "0.1.0-SNAPSHOT"]])
+                 [com.novemberain/monger "1.5.0-rc1"]]
+  :git-dependencies [["https://github.com/hans/flightgear.git"]]
+  :source-paths [".lein-git-deps/flightgear/src"])
